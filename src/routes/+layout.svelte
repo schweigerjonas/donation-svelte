24ddc934fb0eaaf7d03697ddbae694ad8c5b3037
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-03 19:41:59 +0100

    feat: add splash screen component and different images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,3 @@
-<script>
-	import { resolve } from '$app/paths';
-
-	const resolvedRoot = resolve('/');
-	const resolvedLogin = resolve('/login');
-	const resolvedSignup = resolve('/signup');
-</script>
-
 <div class="container">
-	<div class="tabs notification is-primary">
-		<ul>
-			<li><a href={resolvedRoot}>Start</a></li>
-			<li><a href={resolvedLogin}>Login</a></li>
-			<li><a href={resolvedSignup}>Signup</a></li>
-		</ul>
-	</div>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<div class="notification is-primary">Hello from Donation!</div>
+<script>
+	import SplashScreen from '$lib/ui/SplashScreen.svelte';
+</script>
+
+<SplashScreen />
