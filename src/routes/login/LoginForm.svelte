ddc11b96bfe53866e0159705880e59eb24e986c6
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-06 08:36:16 +0100

    feat: add donate route with email display

--- src/routes/login/LoginForm.svelte
+++ src/routes/login/LoginForm.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
+	import { loggedInUser } from "$lib/runes.svelte";
 	import Message from "$lib/ui/Message.svelte";
 	import UserCredentials from "$lib/ui/UserCredentials.svelte";
 
@@ -8,10 +9,13 @@
 	let message = $state("");
 
 	async function login() {
-		const success = false;
+		const success = true;
 		if (success) {
+			loggedInUser.email = email;
 			goto("/donate");
 		} else {
+			email = "";
+			password = "";
 			message = "Invalid credentials";
 		}
 	}
